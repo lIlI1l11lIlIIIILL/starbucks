cb91d8c94b548f8ef05566f7701d0957874dae38
lIlI1l11lIlIIIILL <sso7492@gmail.com>
2023-03-20 15:16:01 +0900

    20230320 공통 모듈 분리 by seongwoo

--- js/index.js
+++ js/index.js
@@ -14,24 +14,7 @@ window.onload = function(){
     }
 }
 
-const searchE1 = document.querySelector(".search_bar");
-const searchInputE1 = searchE1.querySelector("input");
-console.log(searchE1);
-console.log(searchInputE1);
 
-searchE1.addEventListener('click' , function(){
-    searchInputE1.focus();
-});
-
-searchInputE1.addEventListener('focus' , function(){
-    searchE1.classList.add('focused');
-    searchInputE1.setAttribute('placeholder', '검색어를 입력 해주세요.');
-});
-
-searchInputE1.addEventListener('blur' , function(){
-    searchE1.classList.remove('focused');
-    searchInputE1.setAttribute('placeholder', '');
-});
 
 const badgeE1 = document.querySelector('#top_layout .badges');
 
